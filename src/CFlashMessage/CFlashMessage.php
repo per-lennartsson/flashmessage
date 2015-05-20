51b5690d84715bf9b8dd68bf2bd95f3c1f832a57
perlennartsson95 <perlennartsson@outlook.com>
2015-05-20 15:42:14 +0200

    Add src

---
+++ src/CFlashMessage/CFlashMessage.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace flashmessage\CFlashMessage;
+
+class CFlashMessage
+{
+
+    function __construct()
+    {
+        if(!isset($_SESSION['flash'])) {
+        $_SESSION['flash'] = array();
+        }
+    }
+
+    public function clear()
+    {
+        $_SESSION['flash'] = null;
+    }
+
+    public function message($type , $message)
+    {
+
+        $_SESSION['flash'][] = [
+        'type' => $type,
+        'message' => $message,
+        ];
+    }
+
+    public function getMessages()
+    {
+        $messages = null;
+        if(isset($_SESSION['flash'])) {
+            foreach($_SESSION['flash'] as $flashes => $flash) {
+            $type = $flash['type'];
+            $message = $flash['message'];
+            $messages .= "<div class='flash_{$type}'>\n";
+            $messages .= "  " . $message . "\n</div>\n";
+            }
+        $_SESSION['flash'] = null;
+        }
+        return $messages;
+    }
+
+}
