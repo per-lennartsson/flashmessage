80eda82ac416ae55c27ab77dac661cb13d6da532
perlennartsson95 <perlennartsson@outlook.com>
2015-05-20 16:44:32 +0200

    Explain what changes I did

--- webroot/test.php
+++ webroot/test.php
@@ -3,29 +3,24 @@
 require __DIR__.'/config_with_app.php';
 
 $di->setShared('flash', function() {
-    $flash = new \flashmessage\CFlashMessage\CFlashMessage();
+    $flash = new \CFlashMessage\CFlashMessage();
     return $flash;
 });
 
-// Other services, modules, controllers here
-
-// Starts the session (required by the Flashy class)
 $app->session;
 
-// Extra stylesheet
 $app->theme->addStylesheet('css/flash.css');
 
-// Routes
 $app->router->add('', function() use ($app) {
 
     $app->theme->setTitle("Flash test");
 
     $app->session;
 
-    $app->flash->add('notice', 'This is a notice message');
-    $app->flash->add('error', 'This is a error message');
-    $app->flash->add('success', 'This is a success message');
-    $app->flash->add('warning', 'This is a warning message');
+    $app->flash->message('info', 'This is a info message');
+    $app->flash->message('error', 'This is a error message');
+    $app->flash->message('success', 'This is a success message');
+    $app->flash->message('warning', 'This is a warning message');
 
 
     $app->theme->setVariable('title', "Flash test")
